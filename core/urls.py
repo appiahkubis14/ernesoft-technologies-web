2263a396f49f2b20d679657ce95e491919b8c8b2
yamforo <appiahkubis14@gmail.com>
2025-11-05 09:28:45 +0000

    first commit

---
+++ core/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about-us/', views.about, name='about'),
+    path('team/', views.team, name='team'),
+    path('team-details/', views.team_details, name='team_details'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('project/', views.project, name='project'),
+    path('project-details/', views.project_details, name='project_details'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('faq/', views.faq, name='faq'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
+    path('service-details/', views.service_details, name='service_details'),
+    path('blog-grid/', views.blog_grid, name='blog_grid'),
+    path('blog-2column/', views.blog_2column, name='blog_2column'),
+    path('blog-details/', views.blog_details, name='blog_details'),
+
+
+    path('projects/afarisense/', views.pro1, name='pro1'),
+    path('projects/edur-cocoa/', views.pro2, name='pro2'),
+    path('projects/amp-logistics/', views.pro3, name='pro3'),
+    path('projects/human-rights/', views.pro4, name='pro4'),
+    path('projects/eximbank/', views.pro5, name='pro5'),
+    path('projects/kapture-gis/', views.pro6, name='pro6'),
+    path('projects/hcms/', views.pro7, name='pro7'),
+
+    path('services/it/', views.it_service, name='it_service'),
+    path('services/web-development/', views.web_service, name='web_service'),
+    path('services/geospatial/', views.geo_service, name='geo_service'),
+    path('services/mobile-development/', views.mobile_service, name='mobile_service'),
+]
---
+++ core/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def about(request):
+    return render(request, 'core/about-us.html')
+
+def team(request):
+    return render(request, 'core/team.html')
+
+def team_details(request):
+    return render(request, 'core/team-details.html')
+
+def testimonial(request):
+    return render(request, 'core/testimonial.html')
+
+
+def project(request):
+    return render(request, 'core/project.html')
+
+
+def project_details(request):
+    return render(request, 'core/project-details.html')
+
+
+def pricing(request):
+    return render(request, 'core/pricing.html')
+
+
+def faq(request):
+    return render(request, 'core/faq.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def service(request):
+    return render(request, 'core/service.html')
+
+def service_details(request):
+    return render(request, 'core/service-details.html')
+
+def blog_grid(request):
+    return render(request, 'core/blog-grid.html')
+
+def blog_2column(request):
+    return render(request, 'core/blog-2column.html')
+
+def blog_details(request):
+    return render(request, 'core/blog-details.html')
+
+
+
+def pro1(request):
+    return render(request, 'core/projects/pro1.html')
+
+def pro2(request):
+    return render(request, 'core/projects/pro2.html')
+
+def pro3(request):
+    return render(request, 'core/projects/pro3.html')
+
+def pro4(request):
+    return render(request, 'core/projects/pro4.html')
+
+def pro5(request):
+    return render(request, 'core/projects/pro5.html')
+
+def pro6(request):
+    return render(request, 'core/projects/pro6.html')
+
+def pro7(request):
+    return render(request, 'core/projects/pro7.html')
+
+
+
+def it_service(request):
+    return render(request, 'core/services/it.html')
+
+def geo_service(request):
+    return render(request, 'core/services/geo.html')
+
+def web_service(request):
+    return render(request, 'core/services/web.html')
+
+def mobile_service(request):
+    return render(request, 'core/services/mobile.html')
